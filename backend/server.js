dc565cfa1035f763b7c1ed9ba7dcacdfc0882f7a
aravindpulluri116 <pulluriaravind@gmail.com>
2025-05-02 22:24:11 +0530

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,29 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const photoRoutes = require('./routes/photoRoutes');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const photoRoutes = require("./routes/photoRoutes");
 
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://test-picshare-5kgm.vercel.app",
+  })
+);
 app.use(express.json());
-app.use('/uploads', express.static('uploads')); // serve uploaded images
+app.use("/uploads", express.static("uploads")); // serve uploaded images
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log('MongoDB connected'))
-  .catch(err => console.error(err));
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error(err));
 
-app.use('/api/photos', photoRoutes);
+app.use("/api/photos", photoRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
